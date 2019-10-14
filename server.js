2fb9586b97f5ba25e59b24a893306b6df13588e3
Jarred Cole <dj.cole710@gmail.com>
2019-10-14 19:41:00 -0400

    adding smartbrainapi

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'jarredcole',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {res.send(database.users)})
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+
+bcrypt.hash("bacon", null, null, function(err, hash) {
+	// Store hash in your password DB.
+})
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+// 	//res == true
+// });
+
+// bcrypt.compare("veggies", hash, function(err, res) {
+// 	// res = false
+// });
+
+
+
+app.listen(3000, () => {
+	console.log('app is running on port 3000');
+})
+
+
+
+
