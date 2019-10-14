295dc13ce4543327ae5281a3372675ccaf2486d8
Jarred Cole <dj.cole710@gmail.com>
2019-10-14 19:43:12 -0400

    adding controllers to api

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai'); 
+
+const app = new Clarifai.App({
+ apiKey: '0453de78d5964951bc09a5f16e3830bd'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+	handleImage,
+	handleApiCall
+}
