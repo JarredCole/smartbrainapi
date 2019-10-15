b169afa39e6a606ca736b4121be95e36dd89946f
Jarred Cole <dj.cole710@gmail.com>
2019-10-15 02:28:59 -0400

    changed adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-contoured-90293',
-    user : 'jarredcole',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
