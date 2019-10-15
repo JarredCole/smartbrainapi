f311c1d6027d1395d4f0e29223ac6eb1cfcc55fc
Jarred Cole <dj.cole710@gmail.com>
2019-10-15 00:44:52 -0400

    adding port for heroku

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ bcrypt.hash("bacon", null, null, function(err, hash) {
 
 
 
-app.listen(3000, () => {
-	console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
 })
 
 
