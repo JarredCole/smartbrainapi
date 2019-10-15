dd9abacd071024843ca26e68bdb65b245fa5549f
Jarred Cole <dj.cole710@gmail.com>
2019-10-15 00:52:21 -0400

    changing respnse

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {res.send(database.users)})
+app.get('/', (req, res) => {res.send('it is working!')})
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
