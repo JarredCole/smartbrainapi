13f43b8eb29a24a98b96a64e1fdfbda8d6560e35
Jarred Cole <dj.cole710@gmail.com>
2019-10-15 02:50:23 -0400

    took out bcrypt code

--- server.js
+++ server.js
@@ -30,22 +30,6 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-
-bcrypt.hash("bacon", null, null, function(err, hash) {
-	// Store hash in your password DB.
-})
-
-// // Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-// 	//res == true
-// });
-
-// bcrypt.compare("veggies", hash, function(err, res) {
-// 	// res = false
-// });
-
-
-
 app.listen(process.env.PORT || 3000, () => {
 	console.log(`app is running on port ${process.env.PORT}`);
 })
