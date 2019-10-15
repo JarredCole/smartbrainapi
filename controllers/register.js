360975b7264d1784f5c0200531c12a33aab69b04
Jarred Cole <dj.cole710@gmail.com>
2019-10-15 14:50:26 -0400

    edits

--- controllers/register.js
+++ controllers/register.js
@@ -3,7 +3,7 @@
 
 const handleRegister = (req, res, db, bcrypt) => {
 	const { email, name, password } = req.body;
-	if (! email || ! name || !passsword) {
+	if (!email || !name || !passsword) {
 		return res.status(400).json('incorrect form submission');
 	}
 	const hash = bcrypt.hashSync(password);
