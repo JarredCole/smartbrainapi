c01acf7e03a5edb16c8a016ca4c7721f67ba5ff2
Jarred Cole <dj.cole710@gmail.com>
2019-10-16 19:28:03 -0400

    added port variable

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-
+const PORT = process.env.PORT
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
@@ -29,8 +29,8 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, () => {
-	console.log(`app is running on port ${process.env.PORT}`);
+app.listen(PORT || 3000, () => {
+	console.log(`app is running on port ${PORT}`);
 });
 
 
