f7daf1511d13700db563608ec3c49a35b3517c42
Jarred Cole <dj.cole710@gmail.com>
2019-10-17 03:07:29 -0400

    took out the app.get response

--- controllers/register.js
+++ controllers/register.js
@@ -17,7 +17,6 @@ const handleRegister = (db, bcrypt) => (req, res) => {
 		  .insert({
 			email: loginEmail[0],
 			name: name,
-			password: password,
 			joined: new Date()
 		})
 		 .then(user => {
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {res.send('it is working!')})
+
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', register.handleRegister(db, bcrypt))
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
