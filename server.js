073ae6c42e2cdb06c43932ef3be243ef146596f9
Jarred Cole <dj.cole710@gmail.com>
2019-10-17 03:37:02 -0400

    add response back to app.get root

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-
+app.get("/", (req, res) => {res.send('it its working')})
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
